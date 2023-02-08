e450a3f24a1aca87d03ba4fe53cc0c40e3107173
Zachary <104698652+zdhenard42@users.noreply.github.com>
2023-02-08 10:59:53 -0600

    Update colored_ping.ps1

--- Powershell/colored_ping.ps1
+++ Powershell/colored_ping.ps1
@@ -1,4 +1,4 @@
-﻿cls
+cls
 function Ping($test){
     $temp=$test.split(" ")[1]
     $IP=$test.split(" ")[0]
@@ -17,10 +17,49 @@ function Ping($test){
     
 }
 
-while ($true){
-    cls
-    foreach($line in Get-Content .\IP_List.txt) {
-        Ping($line)
+function test($temp){
+    while ($true){
+        cls
+        foreach($line in Get-Content .\$temp) {
+            Ping($line)
+        }
+        sleep(5)
     }
-    sleep(5)
 }
+
+function Show-Menu
+{
+     param (
+           [string]$Title = ‘My Menu’
+     )
+     cls
+     Write-Host “================ $Title ================”
+    
+     Write-Host “1: Press ‘1’ for this option.”
+     Write-Host “2: Press ‘2’ for this option.”
+     Write-Host “3: Press ‘3’ for this option.”
+     Write-Host “Q: Press ‘Q’ to quit.”
+}
+
+do
+{
+     Show-Menu
+     $input = Read-Host “Please make a selection”
+     switch ($input)
+     {
+           ‘1’ {
+                cls
+                test('IP_List.txt')
+           } ‘2’ {
+                cls
+                ‘You chose option #2’
+           } ‘3’ {
+                cls
+                ‘You chose option #3’
+           } ‘q’ {
+                return
+           }
+     }
+     pause
+}
+until ($input -eq ‘q’)
