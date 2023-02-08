028a765449a016fed49c5ec0eb32f4fe0ca7d010
zdhenard42 <zdhenard42@tntech.edu>
2023-02-08 10:19:57 -0600

    Fixed ping

--- Powershell/colored_ping.ps1
+++ Powershell/colored_ping.ps1
@@ -1,12 +1,14 @@
 ﻿cls
-function Ping($IP){
+function Ping($test){
+    $temp=$test.split(" ")[1]
+    $IP=$test.split(" ")[0]
     if ( $IP ) {
         $success = Test-Connection $IP -Count 1 -Quiet -ErrorAction SilentlyContinue
         if ( $success ) {
-            Write-Host -ForegroundColor Green "Ping succeeded for $IP"
+            Write-Host -ForegroundColor Green "Ping succeeded for $IP, $temp"
         }
         else {
-            Write-Host -ForegroundColor Red "Ping failed for $IP"
+            Write-Host -ForegroundColor Red "Ping failed for $IP, $temp"
         }
       }
     else {
