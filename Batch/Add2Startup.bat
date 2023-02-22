97bddafbd7f82cf908687f7e3d1c666c9062abd4
Zachary <104698652+zdhenard42@users.noreply.github.com>
2023-02-21 20:52:29 -0600

    Update Add2Startup.bat

--- Batch/Add2Startup.bat
+++ Batch/Add2Startup.bat
@@ -2,7 +2,7 @@
 
 ::exectute this file in the same dir that the exe is located in!
 
-set file=rawaccel123.exe & ::Enter filename here
+set file=filename.exe & ::Enter filename here
 set path=%~dp0 & :: This is setting the path of the exe to the dir the batch file is executed in
 echo @echo off > "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\runFile.bat"
 echo cd "%path%" >> "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\runFile.bat"
